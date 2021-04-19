2e971b6c21bba005454ad886921b8d6fa2d4b9d9
Rudá Torquato <rudatorquato@gmail.com>
2021-04-19 20:16:47 -0300

    score card

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -11,27 +12,33 @@ class AppBarWidget extends PreferredSize {
               decoration: BoxDecoration(gradient: AppGradients.linear),
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text.rich(TextSpan(
-                        text: "Olá, ",
-                        style: AppTextStyles.title,
-                        children: [
-                          TextSpan(
-                            text: "Rudá",
-                            style: AppTextStyles.titleBold,
-                          )
-                        ])),
-                    Container(
-                      width: 58,
-                      height: 58,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://avatars.githubusercontent.com/u/23511909?v=4"))),
-                    )
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(TextSpan(
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                text: "Rudá",
+                                style: AppTextStyles.titleBold,
+                              )
+                            ])),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://avatars.githubusercontent.com/u/23511909?v=4"))),
+                        )
+                      ],
+                    ),
+                    ScoreCardWidget()
                   ],
                 ),
               ),
