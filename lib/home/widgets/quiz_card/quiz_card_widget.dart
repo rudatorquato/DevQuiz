0ee35c1f9766cbaaa62602249620f3425a5821cb
Rudá Torquato <rudatorquato@gmail.com>
2021-06-01 15:56:56 -0300

    quiz cartd completo sem o onTap

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -9,32 +9,40 @@ class QuizCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(16),
       decoration: BoxDecoration(
           border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
           color: AppColors.white,
           borderRadius: BorderRadius.circular(10)),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 40,
             width: 40,
             child: Image.asset(AppImages.blocks),
           ),
+          SizedBox(
+            height: 24,
+          ),
           Text(
             "Gerenciamento de Estado",
             style: AppTextStyles.heading15,
+          ),
+          SizedBox(
+            height: 24,
           ),
           Row(
             children: [
               Expanded(
                 flex: 1,
                 child: Text(
-                  "3 de 1",
+                  "3 de 10",
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
-                flex: 2,
+                flex: 4,
                 child: LinearProgressIndicator(
                   value: 0.3,
                   backgroundColor: AppColors.chartSecondary,
